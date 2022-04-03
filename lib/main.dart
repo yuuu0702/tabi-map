c04b7be00a4140bed23dda875451c49f736ee2c6
Yudai Watanabe <yudai.0702wa@gmail.com>
2022-04-04 00:27:15 +0900

    ?

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,64 @@
 import 'package:flutter/material.dart';
-import 'widget/app.dart';
 
-void main() => runApp(const MyApp());
+void main() => runApp(const Home());
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  var _selectIndex = 0;
+
+  final _pages = <Widget>[
+    Container(
+      child: const Text('Settings'),
+      alignment: Alignment.center,
+    ),
+    Container(
+      child: const Text('Home'),
+      alignment: Alignment.center,
+      color: Colors.lightBlue,
+    ),
+    Container(
+      child: const Text('Favorite'),
+      alignment: Alignment.center,
+      color: Colors.pink.withOpacity(0.5),
+    ),
+  ];
+
+  void _onTapItem(int index) {
+    setState(() {
+      _selectIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return Scaffold(
+      body: _pages[_selectIndex],
+      bottomNavigationBar: Builder(
+        builder: (context) => BottomNavigationBar(
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.settings),
+              label: 'Settings',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.favorite),
+              label: 'Favorite',
+            ),
+          ],
+          currentIndex: _selectIndex,
+          onTap: _onTapItem,
+        ),
       ),
-      home: const App(),
     );
   }
 }
